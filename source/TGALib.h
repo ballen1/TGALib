427205ae9a9f71cc96c65c38b892dd4608aa4c81
Brandon <brandonreallen@gmail.com>
2017-10-13 22:22:52 -0400

    Fix formatting

--- source/TGALib.h
+++ source/TGALib.h
@@ -3,42 +3,44 @@
 
 #include <string>
 
-struct tga_header {
-  char idlength;
-  char colourmaptype;
-  char datatypecode;
-  short int colormaporigin;
-  short int colormaplength;
-  char colourmapdepth;
-  short int x_origin;
-  short int y_origin;
-  short width;
-  short height;
-  char bitsperpixel;
-  char imagedescriptor;
+struct tga_header
+{
+    char idlength;
+    char colourmaptype;
+    char datatypecode;
+    short int colormaporigin;
+    short int colormaplength;
+    char colourmapdepth;
+    short int x_origin;
+    short int y_origin;
+    short width;
+    short height;
+    char bitsperpixel;
+    char imagedescriptor;
 };
 
-struct tga_pixel {
-  unsigned char r, g, b, a;
+struct tga_pixel
+{
+    unsigned char r, g, b, a;
 };
 
-class tga_image {
+class tga_image
+{
+private:
+    int width;
+    int height;
+    int bytes_per_pixel;
 
- private:
-  int width;
-  int height;
-  int bytes_per_pixel;
+    char* file_data;
+    int file_data_length;
 
-  char* file_data;
-  int file_data_length;
+public:
+    tga_image();
+    tga_image(int w, int h, int bpp);
+    ~tga_image();
 
- public:
-  tga_image();
-  tga_image(int w, int h, int bpp);
-  ~tga_image();
-
-  void load_tga_file(std::string file_path);
-  void save_tga_file(std::string file_path);
+    void load_tga_file(std::string file_path);
+    void save_tga_file(std::string file_path);
 
 };
 
--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,8 @@
 #include "TGALib.h"
 
-int main(int argc, char** argv) {
-  tga_image someImage(5, 5, 5);
-  someImage.load_tga_file("C:\\Users\\bcoder\\Documents\\Cosmo_Kramer.tga");
-  someImage.save_tga_file("C:\\Users\\bcoder\\Documents\\Copy_Kramer.tga");
+int main(int argc, char** argv)
+{
+    tga_image someImage(5, 5, 5);
+    someImage.load_tga_file("C:\\Users\\bcoder\\Documents\\Cosmo_Kramer.tga");
+    someImage.save_tga_file("C:\\Users\\bcoder\\Documents\\Copy_Kramer.tga");
 }
